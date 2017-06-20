3d1727635c6f3474ce56d58f7260bbc3e10f6540
jzhugithub <mfx106825372@163.com>
2017-06-20 11:08:45 +0800

    move generate_gallery to generate_label

--- cmc.py
+++ cmc.py
@@ -3,15 +3,16 @@
 
 import numpy as np
 import random
-from scipy.spatial.distance import pdist, squareform,cdist
-#from generate_label import get_dict_ids_images
+from scipy.spatial.distance import pdist, squareform, cdist
+from generate_label import get_dict_ids_images, generate_gallery
 
 
 def _cmc_core(D, G, P):
     m, n = D.shape
     order = np.argsort(D, axis=0)
-    match = (G[order] == P)         # G[order],按距离排序的矩阵，第一行为top1,从左到右为各plabel对应的glabel
-    return (match.sum(axis=1) * 1.0 / n).cumsum()   # 从上至下累积求和
+    match = (G[order] == P)  # G[order],按距离排序的矩阵，第一行为top1,从左到右为各plabel对应的glabel
+    return (match.sum(axis=1) * 1.0 / n).cumsum()  # 从上至下累积求和
+
 
 def count(distmat, glabels=None, plabels=None, n_selected_labels=None, n_repeat=100):
     """Compute the Cumulative Match Characteristic(CMC)
@@ -58,8 +59,8 @@ def count(distmat, glabels=None, plabels=None, n_selected_labels=None, n_repeat=
     for r in range(n_repeat):
         # Randomly select gallery labels
         ind = np.random.choice(unique_glabels.size,
-                                  n_selected_labels,
-                                  replace=False)
+                               n_selected_labels,
+                               replace=False)
         ind.sort()
         g = unique_glabels[ind]
 
@@ -82,6 +83,7 @@ def count(distmat, glabels=None, plabels=None, n_selected_labels=None, n_repeat=
         ret += _cmc_core(subdist, g, p)
 
     return ret / n_repeat
+
 
 def count_lazy(distfunc, glabels=None, plabels=None, n_selected_labels=None, n_repeat=100):
     """Compute the Cumulative Match Characteristic(CMC) in a lazy manner
@@ -140,8 +142,8 @@ def count_lazy(distfunc, glabels=None, plabels=None, n_selected_labels=None, n_r
     for r in range(n_repeat):
         # Randomly select gallery labels
         ind = np.random.choice(unique_glabels.size,
-                                  n_selected_labels,
-                                  replace=False)
+                               n_selected_labels,
+                               replace=False)
         ind.sort()
         g = unique_glabels[ind]
 
@@ -167,33 +169,6 @@ def count_lazy(distfunc, glabels=None, plabels=None, n_selected_labels=None, n_r
     return ret / n_repeat
 
 
-def generate_gallery(img_dict, n):
-    """
-    generate gallery and probe
-    :param image_dict:  id:image dictionary, a dict
-    :param n:           size of gallery (n different ids), int
-    :return:            [gallery(str list of image names), probe(str list of image names),
-                        glabels（ndarray), plabels(ndarray)]
-    """
-    gallery = []
-    probe = []
-    glabels = []
-    plabels = []
-    for id in img_dict.keys():
-        imgs = img_dict[id]
-        img = random.choice(imgs)       # 对每个id,随机选一张图像放入gallery,剩下放probe
-        imgs.pop(imgs.index(img))
-        gallery.append(img)
-        glabels.append(int(id))
-        for img in imgs:
-            plabels.append(int(id))
-            probe.append(img)
-        if len(glabels) >= n:
-            break
-
-    return gallery, probe, np.array(glabels).astype(np.int32), np.array(plabels).astype(np.int32)
-
-
 def compute_distmat(gallery, probe):
     """
     compute distance of each image feature pairs in gallery and probe, L2
@@ -204,13 +179,13 @@ def compute_distmat(gallery, probe):
     return cdist(gallery, probe)
 
 
+if __name__ == '__main__':
+    # 生成gallery和probe
+    train_eval = get_dict_ids_images()
+    gallery, probe, glabels, plabels = generate_gallery(train_eval, 800)
 
-# 生成gallery和probe
-#train_eval = get_dict_ids_images()
-#gallery, probe, glabels, plabels = generate_gallery(train_eval, 800)
-
-#计算距离矩阵 demo
-#distmat = compute_distmat(np.array([[0, 1],[1,5],[3,2],[3,3]]),np.array([[1,0],[2,3],[1,1]]))
-#计算cmc
-#cmc_mean = count(distmat=distmat,glabels=glabels,plabels=plabels,n_selected_labels=50,n_repeat=10)
-#print('done')
+    # 计算距离矩阵 demo
+    distmat = compute_distmat(np.array([[0, 1], [1, 5], [3, 2], [3, 3]]), np.array([[1, 0], [2, 3], [1, 1]]))
+    # 计算cmc
+    cmc_mean = count(distmat=distmat, glabels=glabels, plabels=plabels, n_selected_labels=50, n_repeat=10)
+    print('done')
