69fb77e899191bcc45980a9e41992259370e3678
jzhugithub <mfx106825372@163.com>
2017-06-27 17:09:57 +0800

    update

--- VGG_model/train.py
+++ VGG_model/train.py
@@ -70,7 +70,7 @@ def train(retain_flag=True, start_step=0):
                                         train_flags.learning_rate_decay_factor,
                                         staircase=True)
         vars_to_optimize = [v for v in tf.trainable_variables() if
-                            (v.name.startswith('fc') | v.name.startswith('conv4'))]
+                            (v.name.startswith('fc') | v.name.startswith('conv__'))]
         print '\nvariables to optimize'
         for v in vars_to_optimize:
             print v.name, v.get_shape().as_list()
@@ -106,6 +106,7 @@ def train(retain_flag=True, start_step=0):
         for step in range(start_step, train_flags.max_step):
             start_time = time.time()
             _, loss_value = sess.run([train_op, loss_mean], feed_dict={train_mode: True, gallery_mode: True})
+            # print pool5.shape
             # _, loss_value, feature = sess.run([train_op, loss_mean, vgg.output], feed_dict={train_mode: True, gallery_mode: True})
             # print('feature abs mean: %s' % (np.mean(np.abs(feature))))
             duration = time.time() - start_time
