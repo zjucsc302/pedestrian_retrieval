5b9c258bf09a1640293828ccd6276262ea2f54ec
jzhugithub <mfx106825372@163.com>
2017-06-30 20:59:49 +0800

    use self-build net

--- VGG_model/vgg19_trainable.py
+++ VGG_model/vgg19_trainable.py
@@ -28,21 +28,22 @@ class Train_Flags():
         self.output_check_point_path = os.path.join(self.current_file_path, 'result', 'check_point')
         self.output_test_features_path = os.path.join(self.current_file_path, 'result', 'test_features')
         self.check_path_exist()
-        self.checkpoint_name = 'model_trip_improve_max.ckpt'
+        self.checkpoint_name = 'trip_improve_mine_norelu.ckpt'
 
-        self.max_step = 1000000
+        self.max_step = 30000
         self.num_per_epoch = 10000
         self.num_epochs_per_decay = 30
-        self.train_batch_size = 8 # image = 3*train_batch_size
+        self.train_batch_size = 16 # image = 3*train_batch_size
         self.test_batch_size = 30
         self.random_train_input_flag = True
 
-        self.output_feature_dim = 512
-        self.initial_learning_rate = 0.00001
+        self.output_feature_dim = 1024
+        self.dropout = 0.9
+        self.initial_learning_rate = 0.0001
         self.learning_rate_decay_factor = 0.9
         self.moving_average_decay = 0.999999
         self.tau1 = 0.4
-        self.tau2 = 0.001
+        self.tau2 = 0.01
         self.beta = 0.002
 
         with open(self.dataset_train_1000_gallery_csv_file_path, 'rb') as f:
@@ -81,6 +82,94 @@ class Vgg19:
         self.var_dict = {}
         self.dropout = dropout
 
+    # # def build(self, rgb_scaled, test_rgb_scaled, train_test_mode):
+    # def build(self, rgb_scaled, train_test_mode):
+    #     """
+    #     load variable from npy to build the VGG
+    #
+    #     :param rgb: rgb image [batch, height, width, 3] values scaled [0, 1]
+    #     :param train_test_mode: a bool tensor, usually a placeholder: if True, dropout will be turned on
+    #     """
+    #
+    #     # Convert RGB to BGR
+    #     red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
+    #     assert red.get_shape().as_list()[1:] == [IMAGE_HEIGHT, IMAGE_WIDTH, 1]
+    #     assert green.get_shape().as_list()[1:] == [IMAGE_HEIGHT, IMAGE_WIDTH, 1]
+    #     assert blue.get_shape().as_list()[1:] == [IMAGE_HEIGHT, IMAGE_WIDTH, 1]
+    #     bgr = tf.concat(axis=3, values=[
+    #         blue - VGG_MEAN[0],
+    #         green - VGG_MEAN[1],
+    #         red - VGG_MEAN[2],
+    #     ])
+    #     assert bgr.get_shape().as_list()[1:] == [IMAGE_HEIGHT, IMAGE_WIDTH, 3]
+    #
+    #     self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
+    #     self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+    #     self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+    #
+    #     self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
+    #     self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+    #     self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+    #
+    #     self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
+    #     self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
+    #     self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
+    #     self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+    #     self.pool3 = self.max_pool(self.conv3_4, 'pool3')
+    #
+    #     self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
+    #     self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
+    #     self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
+    #     self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
+    #     self.pool4 = self.max_pool(self.conv4_4, 'pool4')
+    #     '''
+    #     self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
+    #     self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
+    #     self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
+    #     self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
+    #     self.pool5 = self.max_pool(self.conv5_4, 'pool5')
+    #
+    #     self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 // (2 ** 5)) ** 2) * 512
+    #     self.relu6 = tf.nn.relu(self.fc6)
+    #     if train_mode is not None:
+    #         self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
+    #     elif self.trainable:
+    #         self.relu6 = tf.nn.dropout(self.relu6, self.dropout)
+    #
+    #     self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
+    #     self.relu7 = tf.nn.relu(self.fc7)
+    #     if train_mode is not None:
+    #         self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
+    #     elif self.trainable:
+    #         self.relu7 = tf.nn.dropout(self.relu7, self.dropout)
+    #
+    #     self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
+    #
+    #     self.prob = tf.nn.softmax(self.fc8, name="prob")
+    #
+    #     self.data_dict = None
+    #
+    #     '''
+    #
+    #     # self.fc6 = self.fc_layer(self.pool3, 100352, 256, "fc6_new")
+    #     self.fc6 = self.fc_layer(self.pool4, 50176, 1024, "fc6_new")
+    #     # self.fc6 = self.fc_layer(self.pool5, 14336, 256, "fc6_new")
+    #     # self.fc6 = self.fc_layer(self.pool5, 20480, 256, "fc6_new")
+    #     self.relu6 = tf.nn.relu(self.fc6)
+    #     self.relu6 = tf.cond(train_test_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
+    #
+    #     self.fc7 = self.fc_layer(self.relu6, 1024, 512, "fc7_new")
+    #     # self.relu7 = tf.nn.relu(self.fc7)
+    #     # self.relu7 = tf.cond(train_test_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
+    #
+    #     # l2_normalize
+    #     # for the reason of tf.nn.l2_normalize, input has the same dtype with the output, should be float
+    #     # self.output = tf.cast(self.fc7, dtype=tf.float32)
+    #     # self.output = tf.nn.l2_normalize(self.output, dim=1)
+    #     self.output = self.fc7
+    #
+    #     self.data_dict = None
+
     # def build(self, rgb_scaled, test_rgb_scaled, train_test_mode):
     def build(self, rgb_scaled, train_test_mode):
         """
@@ -102,70 +191,45 @@ class Vgg19:
         ])
         assert bgr.get_shape().as_list()[1:] == [IMAGE_HEIGHT, IMAGE_WIDTH, 3]
 
-        self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
-        self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
-        self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+        self.conv1_0 = tf.layers.conv2d(
+            inputs=bgr,
+            filters=32,
+            kernel_size=[5, 5],
+            strides=(2, 2),
+            padding="same",
+            activation=tf.nn.relu,
+            name='conv1_0')
 
-        self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
-        self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
-        self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+        self.pool1_0 = tf.layers.max_pooling2d(
+            inputs=self.conv1_0,
+            pool_size=[2, 2],
+            strides=2,
+            name='pool1_0')
 
-        self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
-        self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
-        self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
-        self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
-        self.pool3 = self.max_pool(self.conv3_4, 'pool3')
+        self.conv2_0 = tf.layers.conv2d(
+            inputs=self.pool1_0,
+            filters=32,
+            kernel_size=[5, 5],
+            strides=(1, 1),
+            padding="same",
+            activation=tf.nn.relu,
+            name='conv2_0')
 
-        self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
-        self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
-        self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
-        self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
-        self.pool4 = self.max_pool(self.conv4_4, 'pool4')
-        '''
-        self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
-        self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
-        self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
-        self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
-        self.pool5 = self.max_pool(self.conv5_4, 'pool5')
+        self.pool2_0 = tf.layers.max_pooling2d(
+            inputs=self.conv2_0,
+            pool_size=[2, 2],
+            strides=2,
+            name='pool2')
 
-        self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 // (2 ** 5)) ** 2) * 512
-        self.relu6 = tf.nn.relu(self.fc6)
-        if train_mode is not None:
-            self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
-        elif self.trainable:
-            self.relu6 = tf.nn.dropout(self.relu6, self.dropout)
+        self.pool2_flat = tf.reshape(self.pool2_0, [-1, 28 * 14 * 32])
+        # self.fc3_0 = tf.layers.dense(inputs=self.pool2_flat, units=1024, activation=tf.nn.relu, name='fc3_0')
+        self.fc3_0 = tf.layers.dense(inputs=self.pool2_flat, units=1024, name='fc3_0')
+        # self.fc3_0 = self.fc_layer(self.pool2_0, 28 * 14 * 32, 512, "fc3_0")
+        # self.relu3_0 = tf.nn.relu(self.fc3_0)
+        self.fc3_0 = tf.cond(train_test_mode, lambda: tf.nn.dropout(self.fc3_0, self.dropout), lambda: self.fc3_0)
+        # self.fc4_0 = tf.layers.dense(inputs=self.fc3_0, units=512, activation=tf.nn.relu, name='fc4_0')
 
-        self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
-        if train_mode is not None:
-            self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
-        elif self.trainable:
-            self.relu7 = tf.nn.dropout(self.relu7, self.dropout)
-
-        self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
-
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
-
-        self.data_dict = None
-
-        '''
-
-        # self.fc6 = self.fc_layer(self.pool3, 100352, 256, "fc6_new")
-        self.fc6 = self.fc_layer(self.pool4, 50176, 1024, "fc6_new")
-        # self.fc6 = self.fc_layer(self.pool5, 14336, 256, "fc6_new")
-        # self.fc6 = self.fc_layer(self.pool5, 20480, 256, "fc6_new")
-        self.relu6 = tf.nn.relu(self.fc6)
-        self.relu6 = tf.cond(train_test_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
-
-        self.fc7 = self.fc_layer(self.relu6, 1024, 512, "fc7_new")
-        # self.relu7 = tf.nn.relu(self.fc7)
-        # self.relu7 = tf.cond(train_test_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
-
-        # l2_normalize
-        # for the reason of tf.nn.l2_normalize, input has the same dtype with the output, should be float
-        # self.output = tf.cast(self.fc7, dtype=tf.float32)
-        # self.output = tf.nn.l2_normalize(self.output, dim=1)
-        self.output = self.fc7
+        self.output = self.fc3_0
 
         self.data_dict = None
 
@@ -294,13 +358,20 @@ class Vgg19:
             neg = tf.image.decode_jpeg(neg_image, channels=3)
             neg = tf.cast(neg, dtype=tf.int16)
 
-            resized_ref = tf.image.resize_images(ref, (IMAGE_HEIGHT, IMAGE_WIDTH))
-            resized_pos = tf.image.resize_images(pos, (IMAGE_HEIGHT, IMAGE_WIDTH))
-            resized_neg = tf.image.resize_images(neg, (IMAGE_HEIGHT, IMAGE_WIDTH))
+            # resized_ref = tf.image.resize_images(ref, (IMAGE_HEIGHT, IMAGE_WIDTH))
+            # resized_pos = tf.image.resize_images(pos, (IMAGE_HEIGHT, IMAGE_WIDTH))
+            # resized_neg = tf.image.resize_images(neg, (IMAGE_HEIGHT, IMAGE_WIDTH))
+
+            resized_ref = tf.image.resize_images(ref, (IMAGE_HEIGHT, IMAGE_WIDTH + IMAGE_WIDTH / 4))
+            resized_pos = tf.image.resize_images(pos, (IMAGE_HEIGHT, IMAGE_WIDTH + IMAGE_WIDTH / 4))
+            resized_neg = tf.image.resize_images(neg, (IMAGE_HEIGHT, IMAGE_WIDTH + IMAGE_WIDTH / 4))
+            crop_ref = tf.image.crop_to_bounding_box(resized_ref, 0, IMAGE_WIDTH / 8, IMAGE_HEIGHT, IMAGE_WIDTH)
+            crop_pos = tf.image.crop_to_bounding_box(resized_pos, 0, IMAGE_WIDTH / 8, IMAGE_HEIGHT, IMAGE_WIDTH)
+            crop_neg = tf.image.crop_to_bounding_box(resized_neg, 0, IMAGE_WIDTH / 8, IMAGE_HEIGHT, IMAGE_WIDTH)
 
             # generate batch
             trains = tf.train.batch(
-                [resized_ref, resized_pos, resized_neg, order],
+                [crop_ref, crop_pos, crop_neg, order],
                 batch_size=batch_size,
                 capacity=1 + 3 * batch_size
             )
@@ -324,11 +395,13 @@ class Vgg19:
             test_image = tf.image.decode_jpeg(test_file, channels=3)
             test_image = tf.cast(test_image, dtype=tf.int16)
 
-            resized_test = tf.image.resize_images(test_image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+            # resized_test = tf.image.resize_images(test_image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+            resized_test = tf.image.resize_images(test_image, (IMAGE_HEIGHT, IMAGE_WIDTH + IMAGE_WIDTH / 4))
+            crop_test = tf.image.crop_to_bounding_box(resized_test, 0, IMAGE_WIDTH / 8, IMAGE_HEIGHT, IMAGE_WIDTH)
 
             # generate batch
             tests = tf.train.batch(
-                [resized_test, test_image_label, order],
+                [crop_test, test_image_label, order],
                 batch_size=batch_size,
                 capacity=1 + batch_size
             )
