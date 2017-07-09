8828422598282ce59b07876812ffbf3ddaa9bd68
jzhugithub <mfx106825372@163.com>
2017-07-09 20:18:31 +0800

    add predict, train200

--- Res_model/res_data.py
+++ Res_model/res_data.py
@@ -92,60 +92,6 @@ def generate_gallery(img_dict, n):
     return probe, gallery, np.array(plabels).astype(np.int32), np.array(glabels).astype(np.int32)
 
 
-def get_neg_id(ref_id, info_dict):
-    while (True):
-        neg_id = random.sample(info_dict.keys(), 1)[0]
-        if neg_id != ref_id:
-            break
-    return neg_id
-
-
-def get_neg_image(ref_id, info_dict):
-    neg_id = get_neg_id(ref_id, info_dict)
-    neg_list = info_dict[neg_id]
-    neg_image = random.sample(neg_list, 1)[0]
-    return neg_image
-
-
-def get_pos_image(ref_image, ref_id, info_dict):
-    pos_list = info_dict[ref_id]
-    pos_list.remove(ref_image)
-    pos_image = random.sample(pos_list, 1)[0]
-    pos_list.append(ref_image)
-    return pos_image
-
-
-def generate_train_eval(dataset_triplet_pair, csv_path):
-    order = 0
-    with open(csv_path, 'w') as output:
-        for (ref_image, ref_pos_image, ref_neg_image) in dataset_triplet_pair:
-            output.write("%s,%s,%s,%s" % (ref_image, ref_pos_image, ref_neg_image, order))
-            output.write("\n")
-            order += 1
-
-
-def generate_test(gallery_probe, glabels_plabels, test_batch):
-    test_num = 0
-    with open('data/test.csv', 'w') as output:
-        for (image_path, image_label) in zip(gallery_probe, glabels_plabels):
-            output.write("%s,%s" % (image_path, image_label))
-            output.write("\n")
-
-            test_num = test_num + 1
-
-        while (test_num % test_batch != 0):
-            output.write("%s,%s" % (image_path, 'Repeat for batch input'))
-            output.write("\n")
-            test_num = test_num + 1
-
-
-def generate_path_csv(image_path_list, path_csv):
-    with open(path_csv, 'w') as output:
-        for image_path in image_path_list:
-            output.write("%s" % (image_path))
-            output.write("\n")
-
-
 def generate_path_label_order_csv(path_csv, image_path_list, label=None):
     with open(path_csv, 'w') as output:
         if label is None:
@@ -170,14 +116,6 @@ def generate_path_label_order_repeat_probe_csv(path_csv, repeat_num, image_path_
                 for j in range(repeat_num):
                     output.write("%s,%s,%s" % (image_path_list[i], label[i], i))
                     output.write("\n")
-
-
-def generate_name_np(path_np, image_path_list):
-    name_np = np.zeros(len(image_path_list), dtype=np.int32)
-    for i in range(len(image_path_list)):
-        name_np[i] = (image_path_list[i].split('/')[-1].split('.')[0])
-    with open(path_np, "wb") as f:
-        pickle.dump(name_np, f)
 
 
 def generate_name_order(path_csv, image_path_list):
@@ -299,69 +237,41 @@ def generate_path_label():
     X_train_array, X_valid_array = train_test_split(id_path_array, test_size=0.04, random_state=1)
     X_train = array2dict(X_train_array)
     X_valid = array2dict(X_valid_array)
+    print('train id: ' + str(len(X_train)))
+    print('valid id: ' + str(len(X_valid)))
     # generate train file
     generate_id_image_file(X_train, '../data/id_image/id_image_train_%s_%s_%s.pkl', id_num_in_part=100)
+    # generate train_200 file
+    probe_train, gallery_train, plabels_train, glabels_train = generate_gallery(X_train, 200)
+    generate_label_file(plabels_train, './result/test_features/train_200_probe_labels.npy')
+    generate_label_file(glabels_train, './result/test_features/train_200_gallery_labels.npy')
+    generate_image_file(probe_train, '../data/id_image/train_200_probe_image_%s_%s_%s.pkl', len(probe_train))
+    generate_image_file(gallery_train, '../data/id_image/train_200_gallery_image_%s_%s_%s.pkl', len(gallery_train))
+    generate_path_label_order_csv('../data/train_200_probe.csv', probe_train, plabels_train)  # just for count
+    generate_path_label_order_csv('../data/train_200_gallery.csv', gallery_train, glabels_train)  # just for count
+    print('probe_train_200: ' + str(len(probe_train)))
+    print('gallery_train_200: ' + str(len(gallery_train)))
     # generate valid file
     probe_valid, gallery_valid, plabels_valid, glabels_valid = generate_gallery(X_valid, len(X_valid))
     generate_label_file(plabels_valid, './result/test_features/valid_probe_labels.npy')
     generate_label_file(glabels_valid, './result/test_features/valid_gallery_labels.npy')
     generate_image_file(probe_valid, '../data/id_image/valid_probe_image_%s_%s_%s.pkl', len(probe_valid))
     generate_image_file(gallery_valid, '../data/id_image/valid_gallery_image_%s_%s_%s.pkl', len(gallery_valid))
-    generate_path_label_order_csv('../data/valid_probe.csv', probe_valid, plabels_valid) # just for count
-    generate_path_label_order_csv('../data/valid_gallery.csv', gallery_valid, glabels_valid) # just for count
-
-
-    #
-    # generate_image_label_file(probe_valid, plabels_valid, '../data//id_image_train_%s_%s_%s.pkl')
-    #
-    #
-    #
-    #
-    # generate_path_label_order_csv('data/valid_probe.csv', probe_valid, plabels_valid)
-    # generate_path_label_order_csv('data/valid_gallery.csv', gallery_valid, glabels_valid)
-    # print('probe_valid: ' + str(len(probe_valid)))
-    # print('gallery_valid: ' + str(len(gallery_valid)))
-
-
-
-
-
-
-
-    # print('train id: ' + str(len(X_train)))
-    # print('valid id: ' + str(len(X_valid)))
-    # # generate train_triplet_pair csv
-    # dataset_triplet_pair = get_triplet_pair(X_train, 50)
-    # generate_train_eval(dataset_triplet_pair, 'data/train_triplet_pair.csv')
-    # print('train triplet_pair: ' + str(len(dataset_triplet_pair)))
-    # # generate train_1000 csv
-    # probe_train, gallery_train, plabels_train, glabels_train = generate_gallery(X_train, 1000)
-    # generate_path_label_order_csv('data/train_1000_probe.csv', probe_train, plabels_train)
-    # generate_path_label_order_csv('data/train_1000_gallery.csv', gallery_train, glabels_train)
-    # print('probe_train_1000: ' + str(len(probe_train)))
-    # print('gallery_train_1000: ' + str(len(gallery_train)))
-    # # generate classify_1000 csv
-    # generate_classify_csv(X_train, 1000, 'data/train_1000_classify.csv', 'data/valid_1000_classify.csv')
-    # # generate valid csv
-    # probe_valid, gallery_valid, plabels_valid, glabels_valid = generate_gallery(X_valid, len(X_valid))
-    # generate_path_label_order_csv('data/valid_probe.csv', probe_valid, plabels_valid)
-    # generate_path_label_order_csv('data/valid_gallery.csv', gallery_valid, glabels_valid)
-    # generate_path_label_order_repeat_probe_csv('data/valid_repeat_probe.csv', len(gallery_valid), probe_valid,
-    #                                            plabels_valid)
-    # print('probe_valid: ' + str(len(probe_valid)))
-    # print('gallery_valid: ' + str(len(gallery_valid)))
-    # print('repeat_probe_valid: ' + str(len(probe_valid) * len(gallery_valid)))
-    # # generate predict csv
-    # gallery_predict, probe_predict = generate_predict_path()
-    # generate_path_label_order_csv('data/predict_probe.csv', probe_predict)
-    # generate_path_label_order_csv('data/predict_gallery.csv', gallery_predict)
-    # # generate_path_label_order_repeat_probe_csv('data/predict_repeat_probe.csv', len(gallery_predict), probe_predict)
-    # print('prob_predict: ' + str(len(probe_predict)))
-    # print('gallery_predict: ' + str(len(gallery_predict)))
-    # # print('repeat_probe_predict: ' + str(len(probe_predict) * len(gallery_predict)))
-    # # generate predict name csv
-    # generate_name_order('data/predict_probe_name.csv', probe_predict)
-    # generate_name_order('data/predict_gallery_name.csv', gallery_predict)
+    generate_path_label_order_csv('../data/valid_probe.csv', probe_valid, plabels_valid)  # just for count
+    generate_path_label_order_csv('../data/valid_gallery.csv', gallery_valid, glabels_valid)  # just for count
+    print('probe_valid: ' + str(len(probe_valid)))
+    print('gallery_valid: ' + str(len(gallery_valid)))
+    # generate predict csv
+    gallery_predict, probe_predict = generate_predict_path()
+    generate_image_file(probe_predict, '../data/id_image/predict_probe_image_%s_%s_%s.pkl', 2000)
+    generate_image_file(gallery_predict, '../data/id_image/predict_gallery_image_%s_%s_%s.pkl', 2000)
+    generate_path_label_order_csv('data/predict_probe.csv', probe_predict)
+    generate_path_label_order_csv('data/predict_gallery.csv', gallery_predict)
+    print('prob_predict: ' + str(len(probe_predict)))
+    print('gallery_predict: ' + str(len(gallery_predict)))
+    # generate predict name csv
+    generate_name_order('data/predict_probe_name.csv', probe_predict)
+    generate_name_order('data/predict_gallery_name.csv', gallery_predict)
 
 
 if __name__ == '__main__':
